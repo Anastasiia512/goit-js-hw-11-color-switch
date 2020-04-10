d07bcd443e57592441bc15c6c3814147be1cc23b
Anastasiia <nastyona221985@gmail.com>
2020-04-10 15:39:18 +0300

    added color-switch & css

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+import { colors } from "./colors.js";
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const buttonStart = document.querySelector('button[data-action="start"]');
+const buttonStop = document.querySelector('button[data-action="stop"]');
+
+let isActive = false;
+let timer;
+
+const changeColor = function () {
+  if (!isActive) {
+    timer = setInterval(() => {
+      document.body.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length)];
+    });
+  }
+};
+
+const stopChangeColor = function () {
+  clearInterval(timer);
+  isActive = false;
+};
+
+buttonStart.addEventListener("click", changeColor);
+buttonStop.addEventListener("click", stopChangeColor);
